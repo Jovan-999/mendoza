3748f29babac690c498cfb5c4fe00bfb053cddf8
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-05-13 23:53:04 +0200

    in_process

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        gray: '#F7F7F7',
+        orange: '#CA7353',
+      },
+      fontFamily: {
+        secondary: ['Poppins'],
+      },
+      maxWidth: {
+        1400: '1400px',
+      },
+      animation: {
+        infinite:
+          '14.54s linear 1s infinite normal none running marqueeAnimation-2508740',
+      },
+    },
+  },
+  plugins: [],
+};
