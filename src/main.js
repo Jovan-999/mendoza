f5ecd72f691ef48c41215ed39b3d958092f69a4b
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-05-15 23:23:43 +0200

    nearly_finished

---
+++ src/main.js
@@ -0,0 +1,36 @@
+import marquee from 'https://cdn.jsdelivr.net/npm/vanilla-marquee/dist/vanilla-marquee.js';
+
+// new marquee(document.getElementById('marquee'));
+const myScroller = new marquee(document.getElementById('marquee'), {
+  speed: 500,
+  gap: 50,
+  delayBeforeStart: 0,
+  direction: 'left',
+  duplicated: true,
+  pauseOnHover: true,
+});
+const secondScroller = new marquee(document.getElementById('marquee-1'), {
+  speed: 500,
+  gap: 0,
+  delayBeforeStart: 0,
+  direction: 'left',
+  duplicated: true,
+  pauseOnHover: true,
+});
+const thirdScroller = new marquee(document.getElementById('marquee-2'), {
+  speed: 500,
+  gap: 0,
+  delayBeforeStart: 0,
+  direction: 'right',
+  duplicated: true,
+  pauseOnHover: true,
+});
+
+// $('#marquee').marquee({
+//   speed: 5000,
+//   gap: 50,
+//   delayBeforeStart: 0,
+//   direction: 'left',
+//   duplicated: true,
+//   pauseOnHover: true,
+// });
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,23 @@ module.exports = {
         secondary: ['Poppins'],
       },
       maxWidth: {
+        400: '400px',
         1400: '1400px',
+        1920: '1920px',
+      },
+      height: {
+        670: '670px',
+      },
+      backgroundImage: {
+        'usa-map': 'url(/img/USA.svg);',
       },
       animation: {
         infinite:
           '14.54s linear 1s infinite normal none running marqueeAnimation-2508740',
       },
+      // backgroundImage: {
+      //   usa: "url('/img/USA.svg')",
+      // },
     },
   },
   plugins: [],
