ac8236090204c481f400cbc881c6e54882a94e91
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-05-18 22:19:21 +0200

    changes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import marquee from 'https://cdn.jsdelivr.net/npm/vanilla-marquee/dist/vanilla-marquee.js';
 
 const myScroller = new marquee(document.getElementById('marquee'), {
-  speed: 500,
+  speed: 300,
   gap: 50,
   delayBeforeStart: 0,
   direction: 'left',
@@ -9,16 +9,16 @@ const myScroller = new marquee(document.getElementById('marquee'), {
   pauseOnHover: true,
 });
 const secondScroller = new marquee(document.getElementById('marquee-1'), {
-  speed: 500,
-  gap: 0,
+  speed: 300,
+  gap: 50,
   delayBeforeStart: 0,
   direction: 'left',
   duplicated: true,
   pauseOnHover: true,
 });
 const thirdScroller = new marquee(document.getElementById('marquee-2'), {
-  speed: 500,
-  gap: 0,
+  speed: 300,
+  gap: 50,
   delayBeforeStart: 0,
   direction: 'right',
   duplicated: true,
